e01417e7ae976cf0c8264bcb82a757300f5ecb04
sergioreynoso <sergior@gmail.com>
2021-12-29 15:17:04 -0500

    Added header, search bar, dark mode toggle

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 import "./styles/styles.scss";
+
+const body = document.querySelector("body");
+const darkModeToggle = document.querySelector(".dark-mode-toggle");
+const toggleItem = document.querySelectorAll(".dark-mode-toggle__item");
+
+darkModeToggle.addEventListener("click", () => {
+  body.classList.toggle("dark-mode");
+  toggleItem.forEach(item => {
+    item.classList.toggle("hide");
+  });
+});
