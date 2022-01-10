3cb14b2bb5068b0c86c3b021b96da3d1db882d93
sergioreynoso <sergior@gmail.com>
2022-01-10 11:38:28 -0500

    Screen reader announcement when profile is loaded

--- js/searchBar.js
+++ js/searchBar.js
@@ -1,21 +1,32 @@
 const searchForm = document.querySelector(".search-form");
 const searchInput = document.querySelector(".search-form__input");
 const errorMsg = document.querySelector(".search-form__error-message");
+const screenReaderFeedback = document.querySelector(".screen-reader-feedback");
 
 export function addSearchHandler(renderData) {
   searchForm.addEventListener("submit", e => {
     e.preventDefault();
     if (!searchInput.value) {
-      errorMsg.textContent = "Please input a username";
+      const msg = "Please input a username";
+      errorMsg.textContent = msg;
+      updateScreenReaderFeedback(msg);
       return;
     }
     toggleErrorMsg(false);
     renderData(searchInput.value);
+    updateScreenReaderFeedback(`${searchInput.value} profile has loaded`);
   });
 }
 
 export const toggleErrorMsg = isError => {
-  isError ? (errorMsg.textContent = "No results") : (errorMsg.textContent = "");
+  if (!isError) {
+    errorMsg.textContent = "";
+    updateScreenReaderFeedback("");
+    return;
+  }
+  const msg = "No results";
+  errorMsg.textContent = msg;
+  updateScreenReaderFeedback(msg);
 };
 
 searchInput.addEventListener("input", e => {
@@ -25,3 +36,10 @@ searchInput.addEventListener("input", e => {
 searchInput.addEventListener("focus", e => {
   toggleErrorMsg(false);
 });
+
+//Will alert screen reader of status change
+const updateScreenReaderFeedback = msg => {
+  screenReaderFeedback.textContent = `${msg}`;
+};
+
+//Screen reader announcement when profile is loaded
