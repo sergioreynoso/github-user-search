6e59517aa86516b0aa62d2e6f055ffa945616531
sergioreynoso <sergior@gmail.com>
2022-03-30 09:50:46 -0400

    Initial Nextjs migration

--- js/helpers.js
+++
@@ -1,5 +0,0 @@
-// If data comes back as null, populate the element with a default message
-export const footerLinksEmptyState = el => {
-  el.lastElementChild.innerHTML = `Not Available`;
-  el.closest(".footer__item").style.opacity = ".5";
-};
