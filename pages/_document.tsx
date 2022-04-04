abb7166fa57cf6f2ee067e84f89e6b88f291bb34
sergioreynoso <sergior@gmail.com>
2022-04-04 13:38:58 -0400

    Removed commented code

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -49,20 +49,3 @@ export default class MyDocument extends Document {
     );
   }
 }
-
-// export default function Document() {
-//   return (
-//     <Html lang="en">
-//       <Head>
-//         <link
-//           href="https://fonts.googleapis.com/css2?family=Space+Mono:wght@400;700&display=swap"
-//           rel="stylesheet"
-//         />
-//       </Head>
-//       <body>
-//         <Main />
-//         <NextScript />
-//       </body>
-//     </Html>
-//   );
-// }
